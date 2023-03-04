7d98d982844b06adbd1affc8a437592569985fa5
enrique mendez <67400819+EnriqueMendez2108@users.noreply.github.com>
2023-03-03 23:40:17 -0600

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,32 @@
+public class Main {
+    public static void main(String[] args) {
+
+        //Instancia de cuenta de banco
+        CuentaBanco cuentaBanco = new CuentaBanco("Luis Lopez", 1000);
+
+        //ACTIVIDAD:
+        //Retirar 500
+        public double retiro(double cantidad){
+            if (cantidad<= this.saldo){
+                this.saldo = this.saldo - 500;
+                return cantidad;
+            }
+        //Depositar 5,000
+            public double deposito(double cantidad){
+                if (cantidad<= this.saldo){
+                    this.saldo = this.saldo + cantidad;
+                    return cantidad;
+        //Retirar  2,000
+                    public double deposito(double cantidad){
+                        if (cantidad<= this.saldo){
+                            this.saldo = this.saldo + 2000;
+                            return cantidad;
+
+        //Imprime el nombre del propietario de la cuenta
+        System.out.println("Nombre del propietario" + CuentaBanco);
+
+        //Imprime el saldo actual del propietario
+                            System.out.println("Imprime saldo actual " + cantidad);
+
+    }
+}
